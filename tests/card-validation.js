572057085c99116b2d7b11d18ccec0857217def9
stefano <obsidianspam@gmail.com>
2018-10-24 17:18:48 +0100

    Add tests for luhn10

---
+++ tests/card-validation.js
@@ -0,0 +1,76 @@
+// While we should implement the Luhn algorithm manually it seems a good idea
+// to copy the test cases from someone who thought longer about it
+// Freely taken from: https://github.com/braintree/card-validator/blob/master/test/unit/card-number.js
+
+var test = require('tape')
+
+var testCases = [
+  ['', {isValid: false}],
+  ['6', {isValid: false}],
+  ['60', {isValid: false}],
+  ['601', {isValid: false}],
+  ['6011', {isValid: false}],
+  ['4', {isValid: false}],
+  ['41', { isValid: false}],
+  ['411', { isValid: false}],
+  ['', {isValid: false}],
+  ['x', {isValid: false}],
+  ['123', {isValid: false}]
+  ['4012888888881881', { isValid: true}],
+  ['6288997715452584', { isValid: true}],
+  ['6282001509099283', { isValid: true}],
+  ['6269992058134322', { isValid: true}],
+  ['6240008631401148', { isValid: true}],
+  ['4111111111111111', { isValid: true}],
+  ['4111111111111111', { isValid: true}],
+  ['6011000990139424', {isValid: true}],
+  ['411111y', {isValid: false}],
+  ['41111111111111111111', { isValid: false}], // Too long 
+  ['4111 1111 1111 1111', { isValid: true}],
+  ['6011111', {isValid: false}],
+  ['6011111111111117', {isValid: true}]
+  ['2', {isValid: false}],
+  ['27', {isValid: false}],
+  ['272', {isValid: false}],
+  ['2720', {isValid: false}],
+  ['55555555', {isValid: false}],
+  ['5555555555554444', {isValid: true}],
+  ['5555555555554446', {isValid: false}]
+  ['602011', {isValid: false}],
+  ['500000000000', {isValid: false}],
+  ['500000000000061', {isValid: false}],
+  ['5000000000000611', {isValid: true}],
+  ['5000000000000612', {isValid: false}],
+  ['500000000000000005', {isValid: false}],
+  ['5000000000000000005', {isValid: true}],
+  ['5000000000000000001', {isValid: false}],
+  ['50000000000000000009', {isValid: false}]
+  ['3782', {isValid: false}],
+  ['378282246310005', {isValid: true}]
+  ['1', {isValid: false}],
+  ['21', {isValid: false}],
+  ['3530111', {isValid: false}],
+  ['3530111333300000', {isValid: true}]
+  ['6', {isValid: false}],
+  ['62', {isValid: false}],
+  ['623456', { isValid: false}],
+  ['6212345678901232', { isValid: true}],
+  ['', {isValid: false}],
+  ['foo', {isValid: false}],
+  [{}, {isValid: false}],
+  [[], {isValid: false}],
+  ['32908234', {isValid: false}],
+  [32908234, {isValid: false}],
+  [4111111111111111, { isValid: true}],
+  [true, {isValid: false}],
+  [false, {isValid: false}]
+]
+
+test('Luhn10 validation', function (t) {
+  // testCases.map([cardNumber, {isValid}]=>{
+  //   const validation = luhnTest.validate(cardNumber)
+  //   t.equal(validation, isValid, `Test failed on ${cardNumber}`)
+  // })
+  t.fail('Not implemented')
+  t.end()
+})
